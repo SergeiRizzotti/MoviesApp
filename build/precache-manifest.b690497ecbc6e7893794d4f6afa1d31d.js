9d9ad208722a9ec9de45d720892c206d11733997
Sergei <sergei.rizzotti@gmail.com>
2020-04-19 22:27:56 +0300

    add gh pages

---
+++ build/precache-manifest.b690497ecbc6e7893794d4f6afa1d31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "075911ce893a5044762b",
+    "url": "/static/js/main.796a211e.chunk.js"
+  },
+  {
+    "revision": "a918945d3fa5837f6619",
+    "url": "/static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "a918945d3fa5837f6619",
+    "url": "/static/css/2.0ee1b84b.chunk.css"
+  },
+  {
+    "revision": "dab4d5d2de844ba133a8d11803417607",
+    "url": "/index.html"
+  }
+];

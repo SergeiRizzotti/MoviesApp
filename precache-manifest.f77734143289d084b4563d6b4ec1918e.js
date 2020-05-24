fdf756100c8d51d7f608d139b8471b4722dbc6cd
Sergei <sergei.rizzotti@gmail.com>
2020-05-24 16:37:03 +0300

    Updates

---
+++ precache-manifest.f77734143289d084b4563d6b4ec1918e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cac95804670d72d2c5b9",
+    "url": "/MoviesApp/static/js/runtime~main.d18671c3.js"
+  },
+  {
+    "revision": "480f9892b48fd2e2d642",
+    "url": "/MoviesApp/static/js/main.15b72f23.chunk.js"
+  },
+  {
+    "revision": "a918945d3fa5837f6619",
+    "url": "/MoviesApp/static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "a918945d3fa5837f6619",
+    "url": "/MoviesApp/static/css/2.0ee1b84b.chunk.css"
+  },
+  {
+    "revision": "5daf2a63d6936c57262dee259b94af57",
+    "url": "/MoviesApp/index.html"
+  }
+];
